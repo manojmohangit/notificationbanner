f536fb7c04dab2f837440eade082d6a99d664251
MANOJMANI07 <manoj.mohana1011@gmail.com>
2017-12-14 13:19:30 +0530

    Added Grunt workflow to the repository

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify : {
+      options: {
+        mangle: {
+          reserved: ['jQuery']
+        }
+      },
+      dist: {
+        files: {
+          'src/jquery.notificationbanner.min.js': ['src/jquery.notificationbanner.js']
+        }
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('test', ['uglify']);
+};
