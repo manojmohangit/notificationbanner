ec6c369623ba220c483fc11ba7e24f237744f8f5
unknown <manoj.mohana1011@gmail.com>
2017-12-30 01:40:24 +0530

    Updated jQuery to 1.12.1 to support ie8. Basic setup for the plugin is added

--- src/jquery.notificationbanner.js
+++ src/jquery.notificationbanner.js
@@ -1,5 +1,15 @@
+(function($){
 jQuery.fn.notificationBanner = buildNotificatioinBanner;
-
-function buildNotificatioinBanner() {
-  
+function buildNotificatioinBanner(options) {
+	var settings = $.extend({
+					'position':'bottom',
+					'closeButton' : 'enabled'
+					},options); 
+	var outerContainer = jQuery('<div/>',{
+									text : "Notification Banner"
+								}); 		
+	outerContainer.addClass("outer-container");
+	jQuery(outerContainer).appendTo('body');
+	return this;
 }
+}(jQuery));
